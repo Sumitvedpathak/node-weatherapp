66e31d1d2fa86cec66142f659a4ece50f6894096
Sumit <sumitvedpathak@gmail.com>
2021-04-05 08:04:29 -0400

    fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = proce.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 const publicFolder = path.join(__dirname,'../public')
